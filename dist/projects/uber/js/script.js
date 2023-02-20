0d1a55d6fb610c170798db85401e98b4b42bd9fd
Serhii <grammas56@gmail.com>
2023-02-20 15:28:13 +0200

    Uber_maintained

--- dist/projects/uber/js/script.js
+++ dist/projects/uber/js/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.container'),
-    menuItem = document.querySelectorAll('.container_item'),
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
     hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
-        container.classList.toggle('container_active');
+        menu.classList.toggle('menu_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
-            container.classList.toggle('container_active');
+            menu.classList.toggle('menu_active');
         })
     })
 })
